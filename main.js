afae0470c7fa2eaef7c4f16d2b2a732dbb863ed8
Just-xx <kubadreczz@gmail.com>
2023-07-08 00:04:43 +0200

    added paralax

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ import './styles/global.css'
 import './styles/secHero.css'
 import './styles/secHistory.css'
 import './styles/secGeography.css'
+
+
+// PARALAX
+const heroBgFront = document.querySelector('.hero-bg-front');
+
+window.addEventListener('scroll', () => {
+  heroBgFront.style.transform = `translateY(${window.scrollY / 2}px)`;
+})
