f4b397fc6eb471fae68f5f71023091bd6242f5b8
Just-xx <kubadreczz@gmail.com>
2023-07-13 13:48:06 +0200

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     watch: {
       usePolling: true
+    }
+  },
+  root: resolve(__dirname, 'pages'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'pages', 'index.html'),
+        galery: resolve(__dirname, 'pages', 'galery.html'),
+      }
     }
   }
 })
